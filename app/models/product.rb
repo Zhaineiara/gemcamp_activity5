377a6f8b51b1f649c4484e28e3dc8acd95e60480
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-10-07 19:38:59 +0800

    Added validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,10 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates :discount, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, allow_nil: true
+  validates :available, inclusion: { in: [true, false] }
+  validates :released_at, presence: true, allow_nil: true
+  validates :expiry_date, presence: true, comparison: { greater_than: :released_at }, allow_nil: true
 end
